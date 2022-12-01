e429b6fdc8cc53ef90bd0a2cb7393c5e6f4c4f82
Antonio-87 <davydenko.87@list.ru>
2022-12-01 17:39:36 +0300

    add_correct_element_search

--- Javascript_browser/element-search/slider/task.js
+++ Javascript_browser/element-search/slider/task.js
@@ -7,35 +7,41 @@ let i = 0;
 
 lt.onclick = () => {
     slide[i].classList.toggle('slider__item_active');
+    points[i].classList.remove('slider__dot_active');
     i--;
     if (i == -1) {
         i = slide.length - 1
     }  
     slide[i].classList.toggle('slider__item_active');
+    points[i].classList.toggle('slider__dot_active');
 }
 
 gt.onclick = () => {
     slide[i].classList.toggle('slider__item_active');
+    points[i].classList.remove('slider__dot_active');
     i++;
     if (i > slide.length - 1) {
         i = 0
     }
     slide[i].classList.toggle('slider__item_active');
+    points[i].classList.toggle('slider__dot_active');
 }
 
-const duble = [];
+// let duble = [];
 
-points.forEach((i) => {
-    let p = points.indexOf(i);
-    i.onclick = () => {
-        if (duble.length != 0) {
-            duble[0].classList.remove('slider__item_active');
-            slide[p].classList.add('slider__item_active');
-            duble.splice(0, 1, slide[p]);
-        } else {
-            slide[p].classList.add('slider__item_active');
-            duble.splice(0, 1, slide[p]);
-        }
-    }
-})
+// points.forEach((point) => {
+//     point.onclick = (e) => {
+//         let elem = e.target;
+//         if (duble.length != 0) {
+//             duble[0].classList.remove('slider__dot_active');
+//             slide[points.indexOf(elem)].classList.toggle('slider__item_active');
+//             elem.classList.toggle('slider__dot_active');
+//             duble.splice(0, 1, elem);
+//         } else {
+//             slide[points.indexOf(elem)].classList.toggle('slider__item_active');
+//             elem.classList.toggle('slider__dot_active');
+//             duble.splice(0, 1, elem);
+//         }
+//     }
+// });
     
