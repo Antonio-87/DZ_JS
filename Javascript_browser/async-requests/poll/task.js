927d31f56e788173df754dcbd3046b2c0405e2d7
Antonio-87 <davydenko.87@list.ru>
2023-01-03 17:21:32 +0300

    add_async-requests_poll

--- Javascript_browser/async-requests/poll/task.js
+++ Javascript_browser/async-requests/poll/task.js
@@ -8,8 +8,6 @@ fetch(URL).then(response => {
     if (response.ok) {
         response.json().then(resp => {
             poolTitle.insertAdjacentText('afterbegin', resp.data.title);
-            // let id = resp.id;
-            // let indexAnswer;
             let answers = resp.data.answers;
             answers.forEach(el => {
                 poolAnswers.innerHTML += `
@@ -21,44 +19,41 @@ fetch(URL).then(response => {
 
             const poolAnswerList = [...document.querySelectorAll('.poll__answer')];
                 
-                poolAnswerList.forEach(el => {
-                    el.addEventListener('click', () => {
-                        let id = resp.id;
-                        let indexAnswer = poolAnswerList.indexOf(el);
-                        alert('Спасибо, ваш голос засчитан!');
+            poolAnswerList.forEach(el => {
+                el.addEventListener('click', () => {
+                    let id = resp.id;
+                    let indexAnswer = poolAnswerList.indexOf(el);
+                    alert('Спасибо, ваш голос засчитан!');
 
-                        fetch(URL_POST, {
-                            method: 'POST',
-                            headers: {'Content-Type': 'application/json;charset=utf-8'},
-                            body: JSON.stringify({vote: `${id}`, answer: `${indexAnswer}`})
-                        }).then(response => {
-                            if (response.ok) {
-                                response.json().then(resp => {
-                                    poolAnswerList.forEach(el => el.remove());
-                                    let stat = resp.stat;
-                                    let votesList = [];
-                                    stat.forEach(el => votesList.push(el.votes));
-                                    let sumVote = votesList.reduce((a, b) => a + b, 0);
-                                    stat.forEach(el => {
-                                        poolAnswers.innerHTML += `
-                                            <div class="stat__answer">
-                                                ${el.answer} ${Math.round((el.votes * 100)/ sumVote)} %
-                                            </div>
-                                        `
-                                    });
+                    fetch(URL_POST, {
+                        method: 'POST',
+                        headers: {'Content-Type': 'application/json;charset=utf-8'},
+                        body: JSON.stringify({vote: `${id}`, answer: `${indexAnswer}`})
+                    }).then(response => {
+                        if (response.ok) {
+                            response.json().then(resp => {
+                                poolAnswerList.forEach(el => el.remove());
+                                let stat = resp.stat;
+                                let votesList = [];
+                                stat.forEach(el => votesList.push(el.votes));
+                                let sumVote = votesList.reduce((a, b) => a + b, 0);
+                                stat.forEach(el => {
+                                    poolAnswers.innerHTML += `
+                                        <div class="stat__answer">
+                                            ${el.answer} ${Math.round((el.votes * 100)/ sumVote)} %
+                                        </div>
+                                    `
                                 });
-                            } else {
-                                alert('Ошибка в HTTP:' + response.status);
-                            };
-                        });
+                            });
+                        } else {
+                            alert('Ошибка в HTTP:' + response.status);
+                        };
                     });
                 });
+            });
         });
     } else {
         alert('Ошибка в HTTP:' + response.status);
     };
 });
-
-
-
 
