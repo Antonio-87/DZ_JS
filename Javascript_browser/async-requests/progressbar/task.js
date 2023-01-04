c7761e619cab7b60285f3e2182b916f2f91a1984
Antonio-87 <davydenko.87@list.ru>
2023-01-04 12:02:37 +0300

    add_async-requests_progressbar_correct

--- Javascript_browser/async-requests/progressbar/task.js
+++ Javascript_browser/async-requests/progressbar/task.js
@@ -3,17 +3,13 @@ const form = document.getElementById( 'form' );
 
 const URL = 'https://students.netoservices.ru/nestjs-backend/upload';
 
-
-
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const xhr = new XMLHttpRequest(); 
 
-    xhr.addEventListener('progress', () => {
-        if (xhr.readyState === xhr.DONE) {
-            progress.value = e.total/e.loaded;
-        }
+    xhr.upload.addEventListener('progress', (e) => {
+        progress.value = e.total/e.loaded;
     });
     xhr.open('POST', URL);
 
